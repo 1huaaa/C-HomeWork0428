dd1ad721c7f0ae70de13262bdecb916cb538fbd8
SophieChung <sopyiachung0722@gmail.com>
2026-05-06 16:10:29 +0800

    更新

--- ReverseNums2/Program.cs
+++ ReverseNums2/Program.cs
@@ -7,11 +7,11 @@
             Console.Write("請輸入一個整數數字：");
             if (int.TryParse(Console.ReadLine(), out int n))
             {
-                var lines = Enumerable.Range(1, n) // 1到n的序列
-                    .Select(i => new string(
-                        (n - i + 1).ToString()[0], // 顯示數字字元
-                        i                          // 字元重複的次數
-                    ));
+                var lines = Enumerable.Range(1, n)
+                .Select(i => string.Concat(Enumerable.Repeat(
+                    (n - i + 1).ToString(),
+                    i
+                )));
 
                 foreach (var line in lines)
                 {
